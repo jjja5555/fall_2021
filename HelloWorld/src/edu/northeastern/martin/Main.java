e88fec50042c070c0c751767d44ff8e618bfc6b4
MartinH <jjja5555@uw.edu>
2021-09-14 21:18:19 -0700

    hellow world modified

--- HelloWorld/src/edu/northeastern/martin/Main.java
+++ HelloWorld/src/edu/northeastern/martin/Main.java
@@ -1,5 +1,7 @@
 package edu.northeastern.martin;
 
+import java.sql.SQLOutput;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -10,6 +12,29 @@ public class Main {
 
         int numStudent = 62;//原生
         Integer numStudents = 62;//封装
+        System.out.println(numStudents);
+        System.out.println(numStudent);
+
+        int max = Integer.MAX_VALUE;
+        max += 1;
+        int min = Integer.MIN_VALUE;
+        System.out.println(max);
+        System.out.println(min);
+        char firstC  = '\u00A7';
+        System.out.println(firstC);
+        System.out.println(firstC+1);
+
+        long myLong = 5L;
+        long maxLong = Long.MAX_VALUE;
+        long minLong = Long.MIN_VALUE;
+        System.out.println(maxLong);
+        System.out.println(minLong);
+
+        byte maxByte = Byte.MAX_VALUE;
+        byte minByte = Byte.MIN_VALUE;
+        System.out.println(maxByte);
+        System.out.println(minByte);
+
 
 
     }
