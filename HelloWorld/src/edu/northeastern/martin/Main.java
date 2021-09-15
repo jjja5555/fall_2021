21fe55eb60898615e9c1f1040e190ad90ae51337
MartinH <jjja5555@uw.edu>
2021-09-14 20:39:08 -0700

    hellow world modified

--- HelloWorld/src/edu/northeastern/martin/Main.java
+++ HelloWorld/src/edu/northeastern/martin/Main.java
@@ -4,5 +4,13 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hellow World");
+        System.out.println("Hellow World");
+        System.out.println("Hellow World");
+        System.out.println("Hellow World");
+
+        int numStudent = 62;//原生
+        Integer numStudents = 62;//封装
+
+
     }
 }
