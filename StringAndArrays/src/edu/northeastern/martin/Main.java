2a23b60849251c59b556a24afd35079af593e30e
MartinH <jjja5555@uw.edu>
2021-09-28 18:41:42 -0700

    modified

--- StringAndArrays/src/edu/northeastern/martin/Main.java
+++ StringAndArrays/src/edu/northeastern/martin/Main.java
@@ -4,5 +4,24 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        System.out.println(isPalindron("abcba"));
+    }
+
+    private static boolean isPalindron(String input){
+        if(input == null || input.length() <= 1){
+            return true;
+        }
+        char [] arr = input.toCharArray();
+        for(int i = 0; i < arr.length/2;i++){
+            if(arr[i] != arr[arr.length-1-i]){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private static boolean areAnagrams(String str1, String str2){
+
+        return false;
     }
 }
