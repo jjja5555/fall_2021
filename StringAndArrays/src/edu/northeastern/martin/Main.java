3adfc90ec3762b948c14b34838cf81800700cef6
MartinH <jjja5555@uw.edu>
2021-09-28 19:04:57 -0700

    modified

--- StringAndArrays/src/edu/northeastern/martin/Main.java
+++ StringAndArrays/src/edu/northeastern/martin/Main.java
@@ -5,6 +5,8 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println(isPalindron("abcba"));
+        pringDayOfTheWeek(-1);
+        printMonth("January");
     }
 
     private static boolean isPalindron(String input){
@@ -23,5 +25,101 @@ public class Main {
     private static boolean areAnagrams(String str1, String str2){
 
         return false;
+    }
+
+    private static void pringDayOfTheWeek(int day){
+        switch(day){
+            case 0:
+                System.out.println("today is monday");
+                break;
+            case 1:
+                System.out.println("today is monday");
+                break;
+            case 2:
+                System.out.println("today is tuesdsay");
+                break;
+            case 3:
+                System.out.println("today is wednesday");
+                break;
+            case 4:
+                System.out.println("today is thursday");
+                break;
+            case 5:
+                System.out.println("today is friday");
+                break;
+            case 6:
+                System.out.println("today is saturday");
+                break;
+            case 7:
+                System.out.println("today is sunday");
+                break;
+            default:
+                System.out.println("invalid day");
+                break;
+        }
+    }
+
+    static void printMonth(String month){
+        switch(month){
+            case "January":
+                System.out.println(month);
+                break;
+            case "February":
+                System.out.println(month);
+                break;
+            case "March":
+                System.out.println(month);
+                break;
+            case "April":
+                System.out.println(month);
+                break;
+            case "May":
+                System.out.println(month);
+                break;
+            case "June":
+                System.out.println(month);
+                break;
+            case "July":
+                System.out.println(month);
+                break;
+            case "August":
+                System.out.println(month);
+                break;
+            case "September":
+                System.out.println(month);
+                break;
+            case "October":
+                System.out.println(month);
+                break;
+            case "November":
+                System.out.println(month);
+                break;
+            case "December":
+                System.out.println(month);
+                break;
+            default:
+                System.out.println("Not a valid month");
+                break;
+        }
+    }
+
+    static void printSeason(String month){
+        switch(month){
+            case "January", "February", "March":
+                System.out.println("Spring");
+                break;
+            case "April", "May", "June":
+                System.out.println("Summer");
+                break;
+            case "July", "August", "September":
+                System.out.println("Fall");
+                break;
+            case "October", "November", "December":
+                System.out.println("Winter");
+                break;
+            default:
+                System.out.println("Not a valid month");
+                break;
+        }
     }
 }
