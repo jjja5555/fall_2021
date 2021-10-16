841d882b8853099207d924b38c69b6d7735c9d26
MartinH <jjja5555@uw.edu>
2021-10-16 16:43:27 -0700

    Q2 commit and push

---
+++ HW2/Q2/src/edu/northeastern/martin/Sum.java
@@ -0,0 +1,24 @@
+package edu.northeastern.martin;
+
+public class Sum {
+
+    public int add(int first, int second){
+        return first+second;
+    }
+
+    public int add(int first, int second, int third){
+        return first+second+third;
+    }
+
+    public double add(int first, double second){
+        return first+second;
+    }
+
+    public double add(double first, int second){
+        return first+second;
+    }
+
+    public double add(double first, double second){
+        return first+second;
+    }
+}
