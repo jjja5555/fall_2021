ca911f7c99f12f5c16b46eea69a59449fc6f4176
jjja5555 <jjja5555@uw.edu>
2021-10-26 08:17:30 -0700

    Q7

---
+++ codelab/leetcode40/Q7/src/edu/northeastern/martin/Main.java
@@ -0,0 +1,34 @@
+package edu.northeastern.martin;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        int [] nums = {4,5,6,7,0,1,2};
+        System.out.println(search(nums,0));
+    }
+
+    public static int search(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length-1;
+        while(left <= right){
+            int mid = (left+right)/2;
+            if(nums[mid] == target){
+                return mid;
+            }else if(nums[mid] > nums[left]){
+                if(target >= nums[left] && target < nums[mid]){
+                    right = mid-1;
+                }else{
+                    left = mid+1;
+                }
+            }else{
+                if(target > nums[mid] && target <= nums[right]){
+                    left = mid+1;
+                }else{
+                    right = mid-1;
+                }
+            }
+        }
+        return -1;
+    }
+}
